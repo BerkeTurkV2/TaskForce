67fe03b0c7810f166108eba0644f5007458cc6c2
berketurk35 <forcoding2022v2@gmail.com>
2024-01-30 14:53:20 +0300

    LT Test

--- src/pages/Completed/Completed.js
+++ src/pages/Completed/Completed.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 function Completed(){
     return(
         <View>
-            <Text>Selamlar</Text>
+            <Text>Selamlarr</Text>
         </View>
     )
 };
