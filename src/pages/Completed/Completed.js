26e057e9882552be8958d29df0edbc0c95f4cfdb
BerkeLaptop <forcoding2022v2@gmail.com>
2024-01-31 11:36:10 +0300

    Lt test account

--- src/pages/Completed/Completed.js
+++ src/pages/Completed/Completed.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 function Completed(){
     return(
         <View>
-            <Text>Selamlarr</Text>
+            <Text>Selamlarrr test</Text>
         </View>
     )
 };
