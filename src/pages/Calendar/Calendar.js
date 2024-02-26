f518d161b0dd8a0b76353a55f8afa9614c8e806d
Berke <forcoding2022v2@gmail.com>
2024-02-26 11:52:42 +0300

    Up

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -25,39 +25,8 @@ function CalendarPage() {
                 console.error('Error fetching counter list: ', error);
             }
         };
-
         fetchCounterList();
     }, []);
-
-    const calculateTimeDifferenceForItem = (item) => {
-        const currentDate = new Date(); // Şu anki tarih ve saat
-        const selectedDateTime = new Date(item.date); // Öğenin tarih ve saat bilgisi
-        console.log("currentDate", currentDate);
-        console.log("selectedDateTime", selectedDateTime);
-        // Zaman farkını hesapla
-        const timeDifference = selectedDateTime - currentDate;
-        console.log("timeDifference", timeDifference);
-        // Farkı gün, saat ve dakikaya çevir
-        const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hoursDifference = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutesDifference = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-
-        return { days: daysDifference, hours: hoursDifference, minutes: minutesDifference };
-    };
-
-    // Her bir öğe için kalan zamanı ekranda göster
-    const renderTimeDifferenceForItem = (item) => {
-        if (item.date !== '') {
-            const { days, hours, minutes } = calculateTimeDifferenceForItem(item);
-            if (!isNaN(days) && !isNaN(hours) && !isNaN(minutes)) {
-                return `${days} gün ${hours} saat ${minutes} dakika`;
-            } else {
-                return "Geçersiz tarih formatı";
-            }
-        }
-        return '';
-    };
-    
 
     const showDatePicker = () => {
         setDatePickerVisibility(true);
@@ -68,12 +37,51 @@ function CalendarPage() {
     };
 
     const handleConfirmDate = (date) => {
+
+        const currentDate = new Date();
+        let currentDateTime = currentDate.getTime();
+        console.log("currentDateTime:", currentDateTime);
+
+        const selDate = new Date(date);
+        let selDateTime = selDate.getTime();
+        console.log("selDateTime:", selDateTime);
+
+        let timeDiff = selDateTime - currentDateTime;
+        console.log("timeDiff:", timeDiff);
+
         const formattedDate = date.toLocaleDateString('tr-TR', { weekday: 'long', year: 'numeric', month: '2-digit', day: '2-digit' });
         const formattedTime = date.toLocaleTimeString('tr-TR', { hour: '2-digit', minute: '2-digit' });
-
+        
         setSelectedDate(`${formattedDate}, ${formattedTime}`);
+
+        const { days, hours, minutes, seconds } = formatTimeDifference(timeDiff);
+
+        const remainingTime = `${days} gün ${hours} saat ${minutes} dakika ${seconds} saniye`;
+        console.log("remainingTime:", remainingTime);
+
         hideDatePicker();
     };
+
+    const formatTimeDifference = (timeDiff) => {
+        // Milisaniyeleri saniyeye dönüştür
+        let seconds = Math.floor(timeDiff / 1000);
+        // Saniyeleri dakikaya dönüştür
+        let minutes = Math.floor(seconds / 60);
+        // Dakikeleri saate dönüştür
+        let hours = Math.floor(minutes / 60);
+        // Saatleri güne dönüştür
+        let days = Math.floor(hours / 24);
+
+        // Kalan saniyeleri hesapla
+        seconds %= 60;
+        // Kalan dakikeleri hesapla
+        minutes %= 60;
+        // Kalan saatleri hesapla
+        hours %= 24;
+
+        return { days, hours, minutes, seconds };
+    };
+
 
     const openModal = () => {
         setModalVisible(true);
@@ -104,7 +112,7 @@ function CalendarPage() {
             <Text style={styles.underTitle}>Geri Sayım Sayacı</Text>
             <View>
                 {counterList.map((item, index) => (
-                    <CountDownCard key={index} title={item.title} date={item.date} remainingTime={renderTimeDifferenceForItem(item)} />
+                    <CountDownCard key={index} title={item.title} date={item.date} />
                 ))}
             </View>
             <TouchableOpacity style={styles.addIcon} onPress={openModal} >
