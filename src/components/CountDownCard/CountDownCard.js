6e7321c4bce1afa34d0ef54ecccd7dca2c8feb51
Berke <forcoding2022v2@gmail.com>
2024-02-28 17:06:12 +0300

    Up

--- src/components/CountDownCard/CountDownCard.js
+++ src/components/CountDownCard/CountDownCard.js
@@ -1,15 +1,60 @@
-import React from 'react'
-import { View, Text, ImageBackground } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { View, Text } from 'react-native';
 import styles from "./CountDownCardStyles";
 
-function CountDownCard({ title, date, remainingTime}) {
+function CountDownCard({ title, date, test }) {
+
+    const [remainingTime, setRemainingTime] = useState('');
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const currentDate = new Date();
+            let currentDateTime = currentDate.getTime();
+
+            const selDate = new Date(test);
+            let selDateTime = selDate.getTime();
+
+            let timeDiff = selDateTime - currentDateTime;
+
+            const { days, hours, minutes, seconds } = formatTimeDifference(timeDiff);
+
+            const remainingTime = `${days} gün ${hours} saat ${minutes} dakika ${seconds} saniye`;
+        
+            setRemainingTime(remainingTime);
+
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const formatTimeDifference = (timeDiff) => {
+        // Milisaniyeleri saniyeye dönüştür
+        let seconds = Math.floor(timeDiff / 1000);
+        // Saniyeleri dakikaya dönüştür
+        let minutes = Math.floor(seconds / 60);
+        // Dakikeleri saate dönüştür
+        let hours = Math.floor(minutes / 60);
+        // Saatleri güne dönüştür
+        let days = Math.floor(hours / 24);
+
+        // Kalan saniyeleri hesapla
+        seconds %= 60;
+        // Kalan dakikeleri hesapla
+        minutes %= 60;
+        // Kalan saatleri hesapla
+        hours %= 24;
+
+        return { days, hours, minutes, seconds };
+    };
+
+
     return (
         <View style={styles.cardContainer} >
-                <View >
-                    <Text style={styles.title} >{title}</Text>
-                    <Text>tarih : {date}</Text>
-                    <Text>kalan zaman : {remainingTime}</Text>
-                </View>
+            <View >
+                <Text style={styles.title} >{title}</Text>
+                <Text>tarih : {date}</Text>
+                <Text>kalan zaman : {remainingTime}</Text>
+            </View>
         </View>
 
     )
--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -14,6 +14,8 @@ function CalendarPage() {
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
     const [counterList, setCounterList] = useState([]);
 
+    const [test, setTest] = useState('');
+
     useEffect(() => {
         const fetchCounterList = async () => {
             try {
@@ -37,51 +39,15 @@ function CalendarPage() {
     };
 
     const handleConfirmDate = (date) => {
-
-        const currentDate = new Date();
-        let currentDateTime = currentDate.getTime();
-        console.log("currentDateTime:", currentDateTime);
-
-        const selDate = new Date(date);
-        let selDateTime = selDate.getTime();
-        console.log("selDateTime:", selDateTime);
-
-        let timeDiff = selDateTime - currentDateTime;
-        console.log("timeDiff:", timeDiff);
+        setTest(date);
 
         const formattedDate = date.toLocaleDateString('tr-TR', { weekday: 'long', year: 'numeric', month: '2-digit', day: '2-digit' });
         const formattedTime = date.toLocaleTimeString('tr-TR', { hour: '2-digit', minute: '2-digit' });
         
         setSelectedDate(`${formattedDate}, ${formattedTime}`);
 
-        const { days, hours, minutes, seconds } = formatTimeDifference(timeDiff);
-
-        const remainingTime = `${days} gün ${hours} saat ${minutes} dakika ${seconds} saniye`;
-        console.log("remainingTime:", remainingTime);
-
         hideDatePicker();
     };
-
-    const formatTimeDifference = (timeDiff) => {
-        // Milisaniyeleri saniyeye dönüştür
-        let seconds = Math.floor(timeDiff / 1000);
-        // Saniyeleri dakikaya dönüştür
-        let minutes = Math.floor(seconds / 60);
-        // Dakikeleri saate dönüştür
-        let hours = Math.floor(minutes / 60);
-        // Saatleri güne dönüştür
-        let days = Math.floor(hours / 24);
-
-        // Kalan saniyeleri hesapla
-        seconds %= 60;
-        // Kalan dakikeleri hesapla
-        minutes %= 60;
-        // Kalan saatleri hesapla
-        hours %= 24;
-
-        return { days, hours, minutes, seconds };
-    };
-
 
     const openModal = () => {
         setModalVisible(true);
@@ -112,7 +78,7 @@ function CalendarPage() {
             <Text style={styles.underTitle}>Geri Sayım Sayacı</Text>
             <View>
                 {counterList.map((item, index) => (
-                    <CountDownCard key={index} title={item.title} date={item.date} />
+                    <CountDownCard key={index} title={item.title} date={item.date} test={test} />
                 ))}
             </View>
             <TouchableOpacity style={styles.addIcon} onPress={openModal} >
